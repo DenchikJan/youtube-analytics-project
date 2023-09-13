3825b4c9cc2d431f2ab39e2fc62ad3d73483caca
denchikjan <volchkoff.denis29@gmail.com>
2023-09-13 21:52:12 +0300

    chnge init

--- src/channel.py
+++ src/channel.py
@@ -11,14 +11,17 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel_info = youtube.channels().list(id=channel_id, part='brandingSettings,statistics').execute()
+        self.__channel_id = channel_id
+        self.title = self.channel_info['items'][0]['brandingSettings']['channel']['title']
+        self.description = self.channel_info['items'][0]['brandingSettings']['channel']['description']
+        self.url = f"https://www.youtube.com/channel/{self.channel_info['items'][0]['id']}"
+        self.subscriber_count = self.channel_info['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel_info['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel_info = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        channel_info = json.dumps(channel_info, indent=2, ensure_ascii=False)
+        channel_info = json.dumps(self.channel_info, indent=2, ensure_ascii=False)
         print(channel_info)
-
-
-channel = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
